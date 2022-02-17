28e7d80bb8b81816c0ef9d032ee919577a5e0ae7
vanhleg2301 <91180096+vanhleg2301@users.noreply.github.com>
2022-02-17 10:38:11 +0700

    Add files via upload

---
+++ Lập Trình C/Untitled2.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main()
+{
+    int a[MAX_SIZE];
+    int n, value, i;
+    
+    do
+    {
+        printf("Enter an element (zero to stop): ");
+        scanf("%d", &value);
+        if (value)
+            a[n++] = value;
+    } while (value && n < MAX_SIZE - 1);
+        
+    printf("Your entered array: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    
+    return 0;
+}
+
+
+
+// Nhap vao mang n phan tu
+// In ra vi tri va gia tri cua phan tu lon nhat, nho nhat
+// n = 5
+// 6 2 4 7 3
+
+// max: 4, 7
+// min: 2, 2
